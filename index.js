5fa3309f570cacff4575c0ba0d8489e80b4a00eb
rain027 <raincrowsky@gmail.com>
2026-07-01 13:21:38 +0530

    added size limit while registering express.json middleware in inded.js inorder to prevent payload-based DoS attacks.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const helmet = require('helmet')
 
 const app = express()
 app.use(helmet())
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
 
 app.get('/_health',(req,res)=>{
     res.json({status:'ok'})
